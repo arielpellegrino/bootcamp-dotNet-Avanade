172c73be534b388dfd8756ab3d45897168065ba3
Ariel Pellegrino <ariel.pell@gmail.com>
2024-04-30 14:31:08 -0300

    update exercicios foreach

--- ExemploFundamentos/Program.cs
+++ ExemploFundamentos/Program.cs
@@ -1,21 +1,56 @@
 ﻿
 
-int[] arrayInteiros = new int[3];
-
-arrayInteiros[0] = 72;
-arrayInteiros[1] = 64;
-arrayInteiros[2] = 50;
 
 
-//Array.Resize(ref arrayInteiros, arrayInteiros.Length * 2);
 
-int[] arrayInteiroDobrado = new int[arrayInteiros.Length * 2];
-Array.Copy(arrayInteiros, arrayInteiroDobrado, arrayInteiros.Length);
 
-for(int i = 0; i < arrayInteiros.Length; i++)
-{
-    Console.WriteLine(arrayInteiros[i]);
+
 }
+
+
+
+
+
+
+
+
+
+
+/*List<string> listaString = new List<string>();
+
+listaString.Add("SP");
+listaString.Add("BA");
+listaString.Add("MG");
+
+for(int contador =  0; contador < listaString.Count; contador++)
+{
+    Console.WriteLine($"Posição número {contador} - {listaString[contador]}");
+
+}
+
+int contadorForeach - 0;
+foreach(string item in listaString)
+{
+    Console.WriteLine($"Posição número {contadorForeach} - {item}");
+    contadorForeach++;
+}*/
+
+//int[] arrayInteiros = new int[3];
+
+//arrayInteiros[0] = 72;
+//arrayInteiros[1] = 64;
+//arrayInteiros[2] = 50;
+
+
+////Array.Resize(ref arrayInteiros, arrayInteiros.Length * 2);
+
+//int[] arrayInteiroDobrado = new int[arrayInteiros.Length * 2];
+//Array.Copy(arrayInteiros, arrayInteiroDobrado, arrayInteiros.Length);
+
+//for(int i = 0; i < arrayInteiros.Length; i++)
+//{
+//    Console.WriteLine(arrayInteiros[i]);
+//}
 
 // int contadorForEach = 0;
 // foreach(int valor in arrayInteiros)
